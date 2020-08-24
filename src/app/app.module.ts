aa906ffb49e313d45cd81357c1c227dae3d82b76
Javier Ruiz Tommasi <javierrt@yahoo.com>
2020-08-24 15:01:08 -0300

    material 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './modulos/material/material.module'
+import { NavbarComponent } from './componentes/navbar/navbar.component'
+
+import { RouterModule } from '@angular/router'
+
+// Servicios
+import { AuthInterceptor } from './servicios/auth.interceptor'
+import { ErrointerceptorService } from './servicios/errointerceptor.service'
+import { ProductosService } from './servicios/productos.service'
+import { MyProductsService } from './servicios/myproducts.service'
+import { MensajesService } from './servicios/mensajes.service'
+import { UsuariosService } from './servicios/usuarios.service'
+import { ComunicacionService } from './servicios/comunicacion.service'
+import { TendersService } from './servicios/tenders.service'
+import { OffersService } from './servicios/offers.service'
+import { SamplesService } from './servicios/samples.service'
+
+import Rutas from './routes';
 
 @NgModule({
   declarations: [
@@ -10,9 +30,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    NavbarComponent,
+    RouterModule.forRoot(Rutas.config())
   ],
-  providers: [],
+  providers: [
+    ProductosService,
+    MyProductsService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    UsuariosService,
+    ComunicacionService,
+    MensajesService,
+    TendersService,
+    OffersService,
+    SamplesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/routes.ts
@@ -0,0 +1,46 @@
+import { Routes, RouterModule } from '@angular/router'
+
+import { InicioComponent } from './componentes/inicio/inicio.component'
+import { RegisterComponent } from './componentes/register/register.component'
+import { LoginComponent } from './componentes/login/login.component'
+import { ProductosComponent } from './componentes/productos/productos.component'
+import { LicitacionesComponent } from './componentes/licitaciones/licitaciones.component'
+import { MyProductsComponent } from './componentes/myproducts/myproducts.component'
+import { UsuariosComponent } from './componentes/usuarios/usuarios.component'
+import { LogoutComponent } from './componentes/logout/logout.component'
+import { CuentaComponent } from './componentes/cuenta/cuenta.component'
+import { ChangepassComponent } from './componentes/changepass/changepass.component'
+import { OfertasComponent } from './componentes/ofertas/ofertas.component'
+import { SeleccionComponent } from './componentes/seleccion/seleccion.component'
+import { SamplesComponent } from './componentes/samples/samples.component'
+
+class Rutas {
+  private routes: Routes = [
+    { path: 'inicio', component: InicioComponent},
+    { path: 'register', component: RegisterComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'login/:notification', component: LoginComponent},
+    { path: 'productos', component: ProductosComponent},
+    { path: 'licitaciones', component: LicitacionesComponent},
+    { path: 'seleccion', component: SeleccionComponent},
+    { path: 'myproducts', component: MyProductsComponent},
+    { path: 'usuarios', component: UsuariosComponent},
+    { path: 'logout', component: LogoutComponent},
+    { path: 'cuenta', component: CuentaComponent},
+    { path: 'changepass', component: ChangepassComponent},
+    { path: 'ofertas', component: OfertasComponent},
+    { path: 'muestras', component: SamplesComponent},
+    { path: '**', component: InicioComponent}
+  ]
+
+  constructor() {
+    console.log('Rutas OK!')
+  }
+
+  config() {
+    return this.routes
+  }
+
+}
+
+export default new Rutas()
